5558a40d1fc872f6078dbd0a8151344a35702a44
zcbm4321 <zcbm4321@gmail.com>
2023-12-04 07:58:34 +0900

    docker bug

--- src/libs/getpublicip.ts
+++ src/libs/getpublicip.ts
@@ -1,12 +1,11 @@
 import http from 'http'
 
 function GetPublicIp(callback: Function) {
-        /*
     fetch("https://api.ipify.org?format=json", { method: 'GET' })
         .then(res => res.json())
         .then(data => callback(data.ip));
-    */
 
+        /*
     // Set the URL of the request to the ipify API
     const options = {
         host: 'api.ipify.org',
@@ -36,6 +35,7 @@ function GetPublicIp(callback: Function) {
     // Send the request
     req.end();
     return req;
+    */
 }
 
 export { GetPublicIp }
